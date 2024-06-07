39b0ccb4762ea61b38defc62d5c97210d6a55e0d
Abu Bakar <abubakarzafar2029@gmail.com>
2024-06-07 21:18:31 +0500

    Question 448 solved

--- LeetCodes_With_Java/src/_448_FindAllDisappearedInArray.java
+++ LeetCodes_With_Java/src/_448_FindAllDisappearedInArray.java
@@ -1,14 +1,20 @@
 import DataStructures.Swap;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class _448_FindAllDisappearedInArray {
+    private static int j;
+
     public static void main(String[] args) {
 
-        int[] unsortedArray = {1, 4, 3, 2, 7, 8, 2, 3, 1};
+        int[] unsortedArray = {1, 1};
+//        int[] unsortedArray = {1, 4, 3, 2, 7, 8, 2, 3, 1};
+
         sort(unsortedArray);
         System.out.println(Arrays.toString(unsortedArray));
-        System.out.println("Hello java!");
+//        System.out.println("Hello java!");
     }
 
     public static void sort(int[] array) {
@@ -19,6 +25,12 @@ public class _448_FindAllDisappearedInArray {
                 Swap.swap(array, i, correctIndex);
             } else i++;
         }
+
+        List<Integer> disappearedList=new ArrayList<>();
+        for (int j = 0; j < array.length; j++) {
+            if (array[j]-1!=j)disappearedList.add(j+1);
+        }
+        System.out.println(disappearedList);
     }
 
 }
